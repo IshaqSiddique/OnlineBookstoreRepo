c0f8b276f28a770123bb635ae7ca9bb57df91476
IshaqSiddique <ishaq.siddique@gmail.com>
2022-08-08 01:40:09 +0530

    Integrated Swagger UI with Book Store API's

--- onlinebookstore/src/main/java/com/org/onlinebookstore/OnlineBookStoreStarter.java
+++ onlinebookstore/src/main/java/com/org/onlinebookstore/OnlineBookStoreStarter.java
@@ -2,10 +2,30 @@ package com.org.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OnlineBookStoreStarter {
     public static void main(String args[]){
         SpringApplication.run(OnlineBookStoreStarter.class, args);
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select().build();
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("Online Book Store")
+                .description("API's for Online Book Store").build();
+    }
+
 }
